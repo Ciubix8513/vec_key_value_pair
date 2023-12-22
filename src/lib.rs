eaf686a6907717ac29ab65e6bab2b37945cbc5f5
Luna <ciubix8514@gmail.com>
2023-12-22 17:44:18 +0300

    Implement most traits and functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use std::borrow::Borrow;
+use std::{
+    borrow::{Borrow, BorrowMut},
+    collections::HashMap,
+    ops::Index,
+};
 
 mod tests;
 
 ///A drop in replacement for `std::collections::HashMap`
-#[derive(Default, Clone, PartialEq)]
-pub struct VecMap<K, V>
-where
-    K: PartialEq,
-    K: Eq,
-{
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct VecMap<K, V> {
     vec: Vec<(K, V)>,
 }
 pub struct VaccantEntrty<'a, K: std::cmp::Eq, V> {
@@ -177,9 +177,125 @@ where
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct Keys<'a, K, V> {
+    inner: core::slice::Iter<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for Keys<'a, K, V> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next() {
+            Some((k, _)) => Some(k),
+            None => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct IntoKeys<K, V> {
+    inner: std::vec::IntoIter<(K, V)>,
+}
+
+impl<K, V> Iterator for IntoKeys<K, V> {
+    type Item = K;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|i| i.0)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Values<'a, K, V> {
+    inner: core::slice::Iter<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for Values<'a, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next() {
+            Some((_, v)) => Some(v),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ValuesMut<'a, K, V> {
+    inner: core::slice::IterMut<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for ValuesMut<'a, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next() {
+            Some((_, v)) => Some(v),
+            None => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct IntoValues<K, V> {
+    inner: std::vec::IntoIter<(K, V)>,
+}
+
+impl<K, V> Iterator for IntoValues<K, V> {
+    type Item = V;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|i| i.1)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Iter<'a, K, V> {
+    inner: core::slice::Iter<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next() {
+            Some((k, v)) => Some((k, v)),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct IterMut<'a, K, V> {
+    inner: core::slice::IterMut<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for IterMut<'a, K, V> {
+    type Item = (&'a K, &'a mut V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next() {
+            Some((k, v)) => Some((k, v)),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Drain<'a, K, V> {
+    inner: std::vec::Drain<'a, (K, V)>,
+}
+
+impl<K, V> Iterator for Drain<'_, K, V> {
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
 impl<K, V> VecMap<K, V>
 where
-    K: PartialEq,
     K: Eq,
 {
     ///Creates an empty `VecMap`
@@ -217,7 +333,11 @@ where
         }
     }
 
-    pub fn remove(&mut self, k: &K) -> Option<V> {
+    pub fn remove<Q>(&mut self, k: &Q) -> Option<V>
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Eq + ?Sized,
+    {
         let mut ind = None;
         for (index, i) in self.vec.iter().enumerate() {
             if i.0 == *k {
@@ -230,25 +350,55 @@ where
         None
     }
 
-    pub fn get(&self, k: &K) -> Option<&V> {
+    pub fn remove_entry<Q>(&mut self, k: &Q) -> Option<(K, V)>
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Eq + ?Sized,
+    {
+        let mut ind = None;
+        for (index, i) in self.vec.iter().enumerate() {
+            if i.0 == *k {
+                ind = Some(index);
+            }
+        }
+        if let Some(ind) = ind {
+            return Some(self.vec.remove(ind));
+        }
+        None
+    }
+
+    pub fn get<Q>(&self, k: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Eq + ?Sized,
+    {
         for (key, v) in self.vec.iter() {
-            if key == k {
+            if k == key.borrow() {
                 return Some(&v);
             }
         }
         None
     }
 
-    pub fn get_mut(&mut self, k: &K) -> Option<&mut V> {
-        for (key, v) in self.vec.iter_mut() {
-            if key == k {
-                return Some(v);
+    pub fn get_mut<Q>(&mut self, k: &Q) -> Option<&mut V>
+    where
+        K: Borrow<Q>,
+        Q: Eq + ?Sized,
+    {
+        let mut ind = None;
+        for (index, (key, v)) in self.vec.iter().enumerate() {
+            if k == key.borrow() {
+                ind = Some(index);
             }
         }
-        None
+        Some(&mut self.vec.get_mut(ind?)?.1)
     }
 
-    pub fn contains_key(&self, k: &K) -> bool {
+    pub fn contains_key<Q>(&self, k: &Q) -> bool
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Eq + ?Sized,
+    {
         for (key, _) in self.vec.iter() {
             if key == k {
                 return true;
@@ -258,13 +408,17 @@ where
     }
 
     pub fn shrink_to_fit(&mut self) {
-        self.vec.shrink_to_fit()
+        self.vec.shrink_to_fit();
+    }
+
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        self.vec.shrink_to(min_capacity);
     }
 
     pub fn entry<'a>(&'a mut self, key: K) -> Entry<'a, K, V> {
         match {
             let mut val = None;
-            for (index, (map_key, v)) in self.vec.iter().enumerate() {
+            for (index, (map_key, _)) in self.vec.iter().enumerate() {
                 if &key == map_key {
                     val = Some(index);
                     break;
@@ -282,9 +436,182 @@ where
             }),
         }
     }
+    pub fn get_key_value<Q>(&self, k: &Q) -> Option<(&K, &V)>
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Eq + ?Sized,
+    {
+        for i in self.vec.iter() {
+            if &i.0 == k {
+                return Some((&i.0, &i.1));
+            }
+        }
+        None
+    }
 
-    //An iterator over all the keys of the map in order they were added, has time complexity of O(len)
-    // pub fn keys(&self) -> {
-    // self.vec.iter().
-    // }
+    pub fn keys(&self) -> Keys<'_, K, V> {
+        Keys {
+            inner: self.vec.iter(),
+        }
+    }
+
+    pub fn into_keys(self) -> IntoKeys<K, V> {
+        IntoKeys {
+            inner: self.vec.into_iter(),
+        }
+    }
+
+    pub fn values(&self) -> Values<'_, K, V> {
+        Values {
+            inner: self.vec.iter(),
+        }
+    }
+
+    pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
+        ValuesMut {
+            inner: self.vec.iter_mut(),
+        }
+    }
+
+    pub fn into_values(self) -> IntoValues<K, V> {
+        IntoValues {
+            inner: self.vec.into_iter(),
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, K, V> {
+        Iter {
+            inner: self.vec.iter(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, K, V> {
+        IterMut {
+            inner: self.vec.iter_mut(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.vec.is_empty()
+    }
+
+    pub fn drain(&mut self) -> Drain<'_, K, V> {
+        Drain {
+            inner: self.vec.drain(..),
+        }
+    }
+
+    pub fn retain<F>(&mut self, f: F)
+    where
+        F: FnMut(&K, &mut V) -> bool,
+    {
+        let mut f = f;
+        self.vec.retain_mut(|i| f(&i.0, &mut i.1));
+    }
+
+    pub fn clear(&mut self) {
+        self.vec.clear();
+    }
+
+    pub fn hasher<S>(&self) -> &S
+    where
+        S: std::hash::BuildHasher,
+    {
+        unimplemented!("Hasher is not implemented for VecMap");
+    }
+}
+
+impl<'a, K, V> Extend<(&'a K, &'a V)> for VecMap<K, V>
+where
+    K: Eq + Copy,
+    V: Copy,
+{
+    fn extend<T: IntoIterator<Item = (&'a K, &'a V)>>(&mut self, iter: T) {
+        for (k, v) in iter {
+            self.insert(*k, *v);
+        }
+    }
+}
+
+impl<K, V> Extend<(K, V)> for VecMap<K, V>
+where
+    K: Eq,
+{
+    fn extend<T: IntoIterator<Item = (K, V)>>(&mut self, iter: T) {
+        for (k, v) in iter {
+            self.insert(k, v);
+        }
+    }
+}
+
+impl<K, V, const N: usize> From<[(K, V); N]> for VecMap<K, V>
+where
+    K: Eq,
+{
+    fn from(value: [(K, V); N]) -> Self {
+        let mut o = Self::new();
+        for (k, v) in value {
+            o.insert(k, v);
+        }
+        o
+    }
+}
+
+impl<K, V> FromIterator<(K, V)> for VecMap<K, V>
+where
+    K: Eq,
+{
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        let mut o = Self::new();
+        for (k, v) in iter {
+            o.insert(k, v);
+        }
+        o
+    }
+}
+
+impl<K, Q, V> Index<&Q> for VecMap<K, V>
+where
+    K: Eq + Borrow<Q>,
+    Q: Eq + ?Sized,
+{
+    type Output = V;
+
+    fn index(&self, index: &Q) -> &Self::Output {
+        self.get(index).unwrap()
+    }
+}
+
+impl<'a, K, V> IntoIterator for &'a VecMap<K, V> {
+    type Item = (&'a K, &'a V);
+
+    type IntoIter = Iter<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            inner: self.vec.iter(),
+        }
+    }
+}
+
+impl<'a, K, V> IntoIterator for &'a mut VecMap<K, V> {
+    type Item = (&'a K, &'a mut V);
+
+    type IntoIter = IterMut<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IterMut {
+            inner: self.vec.iter_mut(),
+        }
+    }
+}
+
+impl<K, V> PartialEq<HashMap<K, V>> for VecMap<K, V>
+where
+    K: Eq,
+    V: PartialEq,
+{
+    fn eq(&self, other: &HashMap<K, V>) -> bool {
+        other.iter().eq(self.iter())
+    }
 }
